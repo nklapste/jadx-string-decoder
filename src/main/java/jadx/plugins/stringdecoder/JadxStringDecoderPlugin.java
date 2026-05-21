d26fc6519873a1fa452ebeebce83a59c7cb11d75
Nathan Klapstein <nklapstein@gmail.com>
2026-05-21 17:57:22 -0400

    putting settings into groups

--- src/main/java/jadx/plugins/stringdecoder/JadxStringDecoderPlugin.java
+++ src/main/java/jadx/plugins/stringdecoder/JadxStringDecoderPlugin.java
@@ -1,9 +1,19 @@
 package jadx.plugins.stringdecoder;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.swing.BoxLayout;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+
 import jadx.api.plugins.JadxPlugin;
 import jadx.api.plugins.JadxPluginContext;
 import jadx.api.plugins.JadxPluginInfo;
 import jadx.api.plugins.JadxPluginInfoBuilder;
+import jadx.api.plugins.gui.ISettingsGroup;
+import jadx.api.plugins.gui.JadxGuiContext;
+import jadx.api.plugins.options.OptionDescription;
 
 public class JadxStringDecoderPlugin implements JadxPlugin {
 	public static final String PLUGIN_ID = "jadx-string-decoder";
@@ -23,6 +33,10 @@ public class JadxStringDecoderPlugin implements JadxPlugin {
 	@Override
 	public void init(JadxPluginContext context) {
 		context.registerOptions(options);
+		JadxGuiContext guiCtx = context.getGuiContext();
+		if (guiCtx != null) {
+			setupCustomSettingsGroup(guiCtx);
+		}
 		if (options.isEnable()) {
 			context.addPass(new B64DeobfuscatePass(options));
 			context.addPass(new B64FieldInitPass(options));
@@ -30,5 +44,42 @@ public class JadxStringDecoderPlugin implements JadxPlugin {
 				context.addPass(new ByteArrayStringPass(options));
 			}
 		}
+	}
+
+	private void setupCustomSettingsGroup(JadxGuiContext guiCtx) {
+		guiCtx.settings().setCustomSettingsGroup(new ISettingsGroup() {
+			private JPanel panel;
+
+			@Override
+			public String getTitle() {
+				return "String Decoder";
+			}
+
+			@Override
+			public JComponent buildComponent() {
+				if (panel == null) {
+					List<OptionDescription> allOpts = options.getOptionsDescriptions();
+					List<OptionDescription> b64Opts = allOpts.stream()
+							.filter(o -> !o.name().contains("ByteArray"))
+							.collect(Collectors.toList());
+					List<OptionDescription> byteArrayOpts = allOpts.stream()
+							.filter(o -> o.name().contains("ByteArray"))
+							.collect(Collectors.toList());
+
+					JComponent b64Panel = guiCtx.settings()
+							.buildSettingsGroupForOptions("B64 String Decoder", b64Opts)
+							.buildComponent();
+					JComponent byteArrayPanel = guiCtx.settings()
+							.buildSettingsGroupForOptions("Byte Array String Decoder", byteArrayOpts)
+							.buildComponent();
+
+					panel = new JPanel();
+					panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+					panel.add(b64Panel);
+					panel.add(byteArrayPanel);
+				}
+				return panel;
+			}
+		});
 	}
 }
