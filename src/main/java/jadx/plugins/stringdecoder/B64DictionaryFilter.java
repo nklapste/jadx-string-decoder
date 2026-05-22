38a4de47d875eb2e31efd6942974fa42612fd0d6
Nathan Klapstein <nklapstein@gmail.com>
2026-05-22 19:12:40 -0400

    claude code cleanup

--- src/main/java/jadx/plugins/stringdecoder/B64DictionaryFilter.java
+++ src/main/java/jadx/plugins/stringdecoder/B64DictionaryFilter.java
@@ -1,6 +1,7 @@
 package jadx.plugins.stringdecoder;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
@@ -38,7 +39,7 @@ final class B64DictionaryFilter {
 					}
 				}
 			}
-		} catch (java.io.IOException ignored) {
+		} catch (IOException ignored) {
 		}
 		return words;
 	}
